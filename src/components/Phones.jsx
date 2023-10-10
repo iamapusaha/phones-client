71ccdcfe3c579dfbbb206f1203cfcdf261595526
iamapusaha <apusaha392@gmail.com>
2023-10-10 22:53:16 +0600

    load phones data

--- src/components/Phones.jsx
+++ src/components/Phones.jsx
@@ -1,9 +1,11 @@
+import { useLoaderData } from "react-router-dom";
 
 
 const Phones = () => {
+    const phones = useLoaderData()
     return (
         <div>
-
+            <h1>here the all number of data is {phones.length}</h1>
         </div>
     );
 };
